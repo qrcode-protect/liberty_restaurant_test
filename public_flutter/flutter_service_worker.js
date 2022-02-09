eda76bc9f566aedadd74b9f46fe27d5dbe94e71e
alpha SOW <alphaseul@yahoo.fr>
2022-02-09 17:47:08 +0100

    url strategie

--- public_flutter/flutter_service_worker.js
+++ public_flutter/flutter_service_worker.js
@@ -4,9 +4,9 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "version.json": "d89e56d7446d75c2c4c129c5f5222859",
-"index.html": "6300d4ea2b9da4dfaba726241bff25ab",
-"/": "6300d4ea2b9da4dfaba726241bff25ab",
-"main.dart.js": "ce74ae199b944f54916d23b6359c688c",
+"index.html": "f99ae7b7fa4d00b661eff03399e5bd29",
+"/": "f99ae7b7fa4d00b661eff03399e5bd29",
+"main.dart.js": "9ca698cd9a816284c490c5537f1a6cef",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
@@ -14,7 +14,7 @@ const RESOURCES = {
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "manifest.json": "bd927c43550af71636c627febd4b361d",
 "assets/AssetManifest.json": "a07af2739dbdb61b7996f9f0fb30705c",
-"assets/NOTICES": "59585ed9ffcda94fb003d8663c60c8c1",
+"assets/NOTICES": "7b905fbb2c8a89bc2db25740bbb5cd97",
 "assets/FontManifest.json": "dc3d03800ccca4601324923c0b1d6d57",
 "assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "6d342eb68f170c97609e9da345464e5e",
 "assets/fonts/MaterialIcons-Regular.otf": "4e6447691c9509f7acdbf8a931a85ca1",
